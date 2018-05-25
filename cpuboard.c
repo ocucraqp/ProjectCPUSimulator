33c51a7e38bbe848fc59c9fbdbaf8642dc077d2b
ocucraqp <mairu0514m.o@gmail.com>
2018-05-25 14:31:10 +0900

    add new file

---
+++ cpuboard.c
@@ -0,0 +1,25 @@
+/*
+ *	Project-based Learning II (CPU)
+ *
+ *	Program:	instruction set simulator of the Educational CPU Board
+ *	File Name:	cpuboard.c
+ *	Descrioption:	simulation(emulation) of an instruction
+ */
+
+#include	"cpuboard.h"
+
+
+/*=============================================================================
+ *   Simulation of a Single Instruction
+ *===========================================================================*/
+int
+step(Cpub *cpub)
+{
+	/*
+	 *   [ add codes here for the simulation of an instruction ]
+	 *   [ return RUN_STEP or RUN_HALT ]
+	 */
+	return RUN_HALT;
+}
+
+
