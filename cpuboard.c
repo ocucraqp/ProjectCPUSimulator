796d01104a0d1d0ac5fcbc6914dd32afdfdb6499
ocucraqp <mairu0514m.o@gmail.com>
2018-05-28 09:38:15 +0900

    code update

--- cpuboard.c
+++ cpuboard.c
@@ -8,18 +8,158 @@
 
 #include	"cpuboard.h"
 
-
 /*=============================================================================
  *   Simulation of a Single Instruction
  *===========================================================================*/
-int
-step(Cpub *cpub)
-{
-	/*
-	 *   [ add codes here for the simulation of an instruction ]
-	 *   [ return RUN_STEP or RUN_HALT ]
-	 */
-	return RUN_HALT;
+int step(Cpub *cpub) {
+	//   [ return RUN_STEP or RUN_HALT ]
+	Uword order_code = 0;
+
+	order_code = cpub->mem[cpub->pc] & 0xfc;
+	if (order_code == 0x0c) {
+		//000011-- HALT
+		return RUN_HALT;
+	}
+
+	order_code = cpub->mem[cpub->pc] & 0xf8;
+	switch (order_code) {
+	case 0x00: //00000--- NOP
+		break;
+	case 0x10: //00010--- OUT
+		OUT(cpub);
+		break;
+	case 0x18: //00011--- IN
+		IN(cpub);
+		break;
+	case 0x20: //00010--- RCF
+		RCF(cpub);
+		break;
+	case 0x28: //00011--- SCF
+		SCF(cpub);
+		break;
+	}
+
+	order_code = cpub->mem[cpub->pc] & 0xf0;
+	switch (order_code) {
+	case 0x60: //0110---- LD
+		LD(cpub);
+		break;
+	case 0x70: //0111---- ST
+		ST(cpub);
+		break;
+	case 0xb0: //1011---- ADD
+		ADD(cpub);
+		break;
+	case 0x90: //1001---- ADC
+		ADC(cpub);
+		break;
+	case 0xa0: //1010---- SUB
+		SUB(cpub);
+		break;
+	case 0x80: //1000---- SBC
+		SBC(cpub);
+		break;
+	case 0xf0: //1111---- CMP
+		CMP(cpub);
+		break;
+	case 0xe0: //1110---- AND
+		AND(cpub);
+		break;
+	case 0xd0: //1101---- OR
+		OR(cpub);
+		break;
+	case 0xc0: //1100---- EOR
+		EOR(cpub);
+		break;
+	case 0x40: //0100---- Shift or Rotate
+		SRsm(cpub);
+		break;
+	case 0x30: //0011---- Bbc
+		Bbc(cpub);
+		break;
+	}
+
+	order_code = cpub->mem[cpub->pc] & 0xfc;
+	if (order_code == 0x0a) {
+		//00001010 JAL
+		JAL(cpub);
+	}
+	if (order_code == 0x0b) {
+		//00001011 JR
+		JR(cpub);
+	}
+
+	cpub->pc++;
+	return RUN_STEP;
 }
 
+void OUT(Cpub *cpub) {
 
+}
+void IN(Cpub *cpub) {
+
+}
+
+void RCF(Cpub *cpub) {
+
+}
+
+void SCF(Cpub *cpub) {
+
+}
+
+void LD(Cpub *cpub) {
+
+}
+
+void ST(Cpub *cpub) {
+
+}
+
+void ADD(Cpub *cpub) {
+
+}
+
+void ADC(Cpub *cpub) {
+
+}
+
+void SUB(Cpub *cpub) {
+
+}
+
+void SBC(Cpub *cpub) {
+
+}
+
+void CMP(Cpub *cpub) {
+
+}
+
+void AND(Cpub *cpub) {
+
+}
+
+void OR(Cpub *cpub) {
+
+}
+
+void EOR(Cpub *cpub) {
+
+}
+
+void SRsm(Cpub *cpub) {
+
+}
+
+void Bbc(Cpub *cpub) {
+
+}
+
+void JAL(Cpub *cpub) {
+
+}
+
+void JR(Cpub *cpub) {
+
+}
--- cpuboard.h
+++ cpuboard.h
@@ -9,11 +9,10 @@
 /*=============================================================================
  *   Architectural Data Types
  *===========================================================================*/
-typedef signed char	Sword;
-typedef unsigned char	Uword;
-typedef unsigned short	Addr;
-typedef unsigned char	Bit;
-
+typedef signed char Sword;
+typedef unsigned char Uword;
+typedef unsigned short Addr;
+typedef unsigned char Bit;
 
 /*=============================================================================
  *   CPU Board Resources
@@ -22,29 +21,44 @@ typedef unsigned char	Bit;
 #define	IMEMORY_SIZE	256
 
 typedef struct iobuf {
-	Bit	flag;
-	Uword	buf;
+	Bit flag;
+	Uword buf;
 } IOBuf;
 
 typedef struct cpuboard {
-	Uword	pc;
-	Uword	acc;
-	Uword	ix;
-	Bit	cf, vf, nf, zf;
-	IOBuf	*ibuf;
-	IOBuf	obuf;
+	Uword pc;
+	Uword acc;
+	Uword ix;
+	Bit cf, vf, nf, zf;
+	IOBuf *ibuf;
+	IOBuf obuf;
 	/*
 	 *   [ add here the other CPU resources if necessary ]
 	 */
-	Uword	mem[MEMORY_SIZE];	/* 0XX:Program, 1XX:Data */
+	Uword mem[MEMORY_SIZE]; /* 0XX:Program, 1XX:Data */
 } Cpub;
-
 
 /*=============================================================================
  *   Top Function of an Instruction Simulation
  *===========================================================================*/
 #define	RUN_HALT	0
 #define	RUN_STEP	1
-int	step(Cpub *);
-
-
+int step(Cpub *);
+void OUT(Cpub *);
+void IN(Cpub *);
+void RCF(Cpub *);
+void SCF(Cpub *);
+void LD(Cpub *);
+void ST(Cpub *);
+void ADD(Cpub *);
+void ADC(Cpub *);
+void SUB(Cpub *);
+void SBC(Cpub *);
+void CMP(Cpub *);
+void AND(Cpub *);
+void OR(Cpub *);
+void EOR(Cpub *);
+void SRsm(Cpub *);
+void Bbc(Cpub *);
+void JAL(Cpub *);
+void JR(Cpub *);
