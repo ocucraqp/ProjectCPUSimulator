ea7dbc97329a6b01c3fd9b219898247202dcd3d6
Unknown <mairu0514m.o@gmail.com>
2018-06-01 04:39:59 +0900

    update 3 functions and add
    update ADD(), JudgeArgA() and JudgeArgB().
    add OUT(), IN(), ADC(), SUB(), SBC(), CMP(), AND(), OR(), EOR(), ReadNextPC() and SetFlag().

--- cpuboard.c
+++ cpuboard.c
@@ -12,193 +12,325 @@
  *   Simulation of a Single Instruction
  *===========================================================================*/
 int step(Cpub *cpub) {
-    //aaaaa//
-    //   [ return RUN_STEP or RUN_HALT ]
-    Uword order_code = 0;
-    order_code = cpub->mem[cpub->pc] & 0xfc;
-    if (order_code == 0x0c) {
-        //000011-- HALT
-        return RUN_HALT;
-    }
+	//aaaaa//
+	//   [ return RUN_STEP or RUN_HALT ]
+	Uword order_code = 0;
+	order_code = cpub->mem[cpub->pc] & 0xfc;
+	if (order_code == 0x0c) {
+		//000011-- HALT
+		return RUN_HALT;
+	}
 
-    order_code = cpub->mem[cpub->pc] & 0xf8;
-    switch (order_code) {
-        case 0x00: //00000--- NOP
-            break;
-        case 0x10: //00010--- OUT
-            OUT(cpub);
-            break;
-        case 0x18: //00011--- IN
-            IN(cpub);
-            break;
-        case 0x20: //00010--- RCF
-            RCF(cpub);
-            break;
-        case 0x28: //00011--- SCF
-            SCF(cpub);
-            break;
-    }
+	order_code = cpub->mem[cpub->pc] & 0xf8;
+	switch (order_code) {
+	case 0x00: //00000--- NOP
+		break;
+	case 0x10: //00010--- OUT
+		OUT(cpub);
+		break;
+	case 0x18: //00011--- IN
+		IN(cpub);
+		break;
+	case 0x20: //00010--- RCF
+		RCF(cpub);
+		break;
+	case 0x28: //00011--- SCF
+		SCF(cpub);
+		break;
+	}
 
-    order_code = cpub->mem[cpub->pc] & 0xf0;
-    switch (order_code) {
-        case 0x60: //0110---- LD
-            LD(cpub);
-            break;
-        case 0x70: //0111---- ST
-            ST(cpub);
-            break;
-        case 0xb0: //1011---- ADD
-            ADD(cpub);
-            break;
-        case 0x90: //1001---- ADC
-            ADC(cpub);
-            break;
-        case 0xa0: //1010---- SUB
-            SUB(cpub);
-            break;
-        case 0x80: //1000---- SBC
-            SBC(cpub);
-            break;
-        case 0xf0: //1111---- CMP
-            CMP(cpub);
-            break;
-        case 0xe0: //1110---- AND
-            AND(cpub);
-            break;
-        case 0xd0: //1101---- OR
-            OR(cpub);
-            break;
-        case 0xc0: //1100---- EOR
-            EOR(cpub);
-            break;
-        case 0x40: //0100---- Shift or Rotate
-            SRsm(cpub);
-            break;
-        case 0x30: //0011---- Bbc
-            Bbc(cpub);
-            break;
-    }
+	order_code = cpub->mem[cpub->pc] & 0xf0;
+	switch (order_code) {
+	case 0x60: //0110---- LD
+		LD(cpub);
+		break;
+	case 0x70: //0111---- ST
+		ST(cpub);
+		break;
+	case 0xb0: //1011---- ADD
+		ADD(cpub);
+		break;
+	case 0x90: //1001---- ADC
+		ADC(cpub);
+		break;
+	case 0xa0: //1010---- SUB
+		SUB(cpub);
+		break;
+	case 0x80: //1000---- SBC
+		SBC(cpub);
+		break;
+	case 0xf0: //1111---- CMP
+		CMP(cpub);
+		break;
+	case 0xe0: //1110---- AND
+		AND(cpub);
+		break;
+	case 0xd0: //1101---- OR
+		OR(cpub);
+		break;
+	case 0xc0: //1100---- EOR
+		EOR(cpub);
+		break;
+	case 0x40: //0100---- Shift or Rotate
+		SRsm(cpub);
+		break;
+	case 0x30: //0011---- Bbc
+		Bbc(cpub);
+		break;
+	}
 
-    order_code = cpub->mem[cpub->pc] & 0xfc;
-    if (order_code == 0x0a) {
-        //00001010 JAL
-        JAL(cpub);
-    }
-    if (order_code == 0x0b) {
-        //00001011 JR
-        JR(cpub);
-    }
+	order_code = cpub->mem[cpub->pc] & 0xfc;
+	if (order_code == 0x0a) {
+		//00001010 JAL
+		JAL(cpub);
+	}
+	if (order_code == 0x0b) {
+		//00001011 JR
+		JR(cpub);
+	}
 
-    cpub->pc++;
-    return RUN_STEP;
+	cpub->pc++;
+	return RUN_STEP;
 }
 
 void OUT(Cpub *cpub) {
-
+	cpub->obuf.buf = cpub->acc;
+	cpub->obuf.flag = 1;
 }
 
 void IN(Cpub *cpub) {
-
+	cpub->acc = cpub->ibuf->buf;
+	cpub->obuf.flag = 0;
 }
 
 void RCF(Cpub *cpub) {
-
+	cpub->cf = 0;
 }
 
 void SCF(Cpub *cpub) {
-
+	cpub->cf = 1;
 }
 
 void LD(Cpub *cpub) {
-
+	//todo
 }
 
 void ST(Cpub *cpub) {
-
+	//todo
 }
 
 void ADD(Cpub *cpub) {
-    int* argA, argB;
-    argA = JudgeArgA(cpub);
-    argB = JudgeArgB(cpub);
-
-
+	Uword *argA, *argB;
+	Uword result;
+	argA = JudgeArgA(cpub);
+	argB = JudgeArgB(cpub);
+	result = *argA + *argB;
+	SetFlag(cpub, *argA, result, 2, 3, 3, 3);
+	if (cpub->cf) {
+		result -= 0x100;
+	}
+	*argA = result;
 }
 
 void ADC(Cpub *cpub) {
-
+	Uword *argA, *argB;
+	Uword result;
+	argA = JudgeArgA(cpub);
+	argB = JudgeArgB(cpub);
+	result = *argA + *argB + cpub->cf;
+	SetFlag(cpub, *argA, result, 3, 3, 3, 3);
+	if (cpub->cf) {
+		result -= 0x100;
+	}
+	*argA = result;
 }
 
 void SUB(Cpub *cpub) {
-
+	Uword *argA, *argB;
+	Uword result;
+	argA = JudgeArgA(cpub);
+	argB = JudgeArgB(cpub);
+	result = *argA - *argB;
+	SetFlag(cpub, *argA, result, 2, 3, 3, 3);
+	if (cpub->cf) {
+		result -= 0x100;
+	}
+	*argA = result;
 }
 
 void SBC(Cpub *cpub) {
-
+	Uword *argA, *argB;
+	Uword result;
+	argA = JudgeArgA(cpub);
+	argB = JudgeArgB(cpub);
+	result = *argA + *argB;
+	SetFlag(cpub, *argA, result, 3, 3, 3, 3);
+	if (cpub->cf) {
+		result -= 0x100;
+	}
+	*argA = result;
 }
 
 void CMP(Cpub *cpub) {
-
+	Uword *argA, *argB;
+	Uword result;
+	argA = JudgeArgA(cpub);
+	argB = JudgeArgB(cpub);
+	result = *argA - *argB;
+	SetFlag(cpub, *argA, result, 2, 3, 3, 3);
 }
 
 void AND(Cpub *cpub) {
-
+	Uword *argA, *argB;
+	Uword result;
+	argA = JudgeArgA(cpub);
+	argB = JudgeArgB(cpub);
+	result = *argA & *argB;
+	SetFlag(cpub, *argA, result, 2, 0, 3, 3);
+	*argA = result;
 }
 
 void OR(Cpub *cpub) {
-
+	Uword *argA, *argB;
+	Uword result;
+	argA = JudgeArgA(cpub);
+	argB = JudgeArgB(cpub);
+	result = *argA | *argB;
+	SetFlag(cpub, *argA, result, 2, 0, 3, 3);
+	*argA = result;
 }
 
 void EOR(Cpub *cpub) {
-
+	Uword *argA, *argB;
+	Uword result;
+	argA = JudgeArgA(cpub);
+	argB = JudgeArgB(cpub);
+	result = *argA ^ *argB;
+	SetFlag(cpub, *argA, result, 2, 0, 3, 3);
+	*argA = result;
 }
 
 void SRsm(Cpub *cpub) {
-
+	//todo
 }
 
 void Bbc(Cpub *cpub) {
-
+	//todo
 }
 
 void JAL(Cpub *cpub) {
-
+	//todo
 }
 
 void JR(Cpub *cpub) {
-
+	//todo
 }
 
-int* JudgeArgA(Cpub *cpub){
-    order_code = cpub->mem[cpub->pc] & 0x08;
-    //ACC or IX
-    if (order_code == 0x00) {
-        return &cpub->acc;
-    }else{
-        return &cpub->ix;
-    }
+Uword *JudgeArgA(Cpub *cpub) {
+	Uword order_code = 0;
+	order_code = cpub->mem[cpub->pc] & 0x08;
+	//ACC or IX
+	if (order_code == 0x00) {
+		return &cpub->acc;
+	} else {
+		return &cpub->ix;
+	}
 }
 
-int* JudgeArgB(Cpub *cpub){
-    order_code = cpub->mem[cpub->pc] & 0x07;
-    //ACC or IX
-    switch (order_code){
-        case 0x00:
-            //000 ACC
-            return &cpub->acc;
-        case 0x01:
-            //001 IX
-            return &cpub->ix;
-        case 0x02:
-        case 0x03:
-            //01- d
-            cpub->pc++;
-            return ReadNextPC(cpub);
-        case 0x04:
-            //100 [d]
-            cpub->pc++;
-            return ReadNextPC(cpub);
+Uword *JudgeArgB(Cpub *cpub) {
+	Uword order_code = 0;
+	order_code = cpub->mem[cpub->pc] & 0x07;
+	//ACC or IX
+	switch (order_code) {
+	case 0x00:
+		//000 ACC
+		return &cpub->acc;
+	case 0x01:
+		//001 IX
+		return &cpub->ix;
+	case 0x02:
+	case 0x03:
+		//01- d
+		return ReadNextPC(cpub, 2);
+	case 0x04:
+		//100 [d]
+		return ReadNextPC(cpub, 4);
+	case 0x05:
+		//101 (d)
+		return ReadNextPC(cpub, 5);
+	case 0x06:
+		//110 [IX+d]
+		return ReadNextPC(cpub, 6);
+	case 0x07:
+		//111 (IX+d)
+		return ReadNextPC(cpub, 7);
+	default:
+		/* never reach here */
+		return 0;
+	}
+}
 
-            //todo 処理
-    }
+Uword *ReadNextPC(Cpub *cpub, int addrMode) {
+	/* addrMode
+	 * 2:d		Immediate address
+	 * 4:[d]	Absolute address (program area)
+	 * 5:(d)	Absolute address (data area)
+	 * 6:[IX+d]	IX qualified address (program area)
+	 * 7:(IX+d)	IX qualified address (data area)
+	 * */
+
+	cpub->pc++;
+
+	switch (addrMode) {
+	case 2:
+		return &cpub->mem[cpub->pc];
+	case 4:
+		return &cpub->mem[cpub->mem[cpub->pc]];
+	case 5:
+		return &cpub->mem[cpub->mem[cpub->pc] + 0x100];
+	case 6:
+		return &cpub->mem[cpub->ix + cpub->mem[cpub->pc]];
+	case 7:
+		return &cpub->mem[cpub->ix + cpub->mem[cpub->pc] + 0x100];
+	default:
+		return 0;
+	}
+}
+
+void SetFlag(Cpub *cpub, Uword argA, Uword flagSource, int cfFlag, int vfFlag,
+		int nfFlag, int zfFlag) {
+	/* xfFlag
+	 * 0:	reset
+	 * 1:	set
+	 * 2:	no effect
+	 * 3:	Depends on argA and flagSource
+	 * */
+
+	//set/reset CF
+	if (flagSource >= 0x100) {
+		cpub->cf = 1;
+	} else {
+		cpub->cf = 0;
+	}
+
+	//set/reset VF
+	if ((argA & 0x80) ^ (flagSource & 0x80)) {
+		cpub->vf = 1;
+	} else {
+		cpub->vf = 0;
+	}
+
+	//set/reset NF
+	if ((flagSource & 0x80) == 0x80) {
+		cpub->vf = 1;
+	} else {
+		cpub->vf = 0;
+	}
+
+	//set/reset ZF
+	if (flagSource == 0x00) {
+		cpub->zf = 1;
+	} else {
+		cpub->zf = 0;
+	}
 }
