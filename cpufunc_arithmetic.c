15912bbb640974f30e4e1eb96453854b26756287
ocucraqp <mairu0514m.o@gmail.com>
2018-06-08 03:15:17 +0900

    update ADC() and SBC()

--- cpufunc_arithmetic.c
+++ cpufunc_arithmetic.c
@@ -9,63 +9,63 @@
 #include "cpuboard.h"
 
 void ADD(Cpub *cpub) {
-	Uword *argA, *argB;
-	int result = 0;
-	argA = JudgeArgA(cpub);
-	argB = JudgeArgB(cpub);
-	result = *argA + *argB;
-	SetFlag(cpub, *argA, *argB, result, -1, 2, 2, 2);
-	*argA = result;
+    Uword *argA, *argB;
+    int result = 0;
+    argA = JudgeArgA(cpub);
+    argB = JudgeArgB(cpub);
+    result = *argA + *argB;
+    SetFlag(cpub, *argA, *argB, result, -1, 2, 2, 2);
+    *argA = result;
 }
 
 void ADC(Cpub *cpub) {
-	Uword *argA, *argB;
-	int result = 0;
-	argA = JudgeArgA(cpub);
-	argB = JudgeArgB(cpub);
-	result = *argA + *argB + cpub->cf;
-	SetFlag(cpub, *argA, *argB, result, 2, 2, 2, 2);
-	if (cpub->cf) {
-		result -= 0x100;
-	}
-	*argA = result;
+    Uword *argA, *argB;
+    int result = 0;
+    argA = JudgeArgA(cpub);
+    argB = JudgeArgB(cpub);
+    result = *argA + *argB + cpub->cf;
+    SetFlag(cpub, *argA, *argB + cpub->cf, result, 2, 2, 2, 2);
+    if (cpub->cf) {
+        result -= 0x100;
+    }
+    *argA = result;
 }
 
 void SUB(Cpub *cpub) {
-	Uword *argA, *argB;
-	Uword argB2;
-	int result = 0;
-	argA = JudgeArgA(cpub);
-	argB = JudgeArgB(cpub);
-	argB2 = Convert2sComplement(*argB);
-	result = *argA + argB2;
-	SetFlag(cpub, *argA, argB2, result, -1, 2, 2, 2);
-	*argA = result;
+    Uword *argA, *argB;
+    Uword argB2;
+    int result = 0;
+    argA = JudgeArgA(cpub);
+    argB = JudgeArgB(cpub);
+    argB2 = Convert2sComplement(*argB);
+    result = *argA + argB2;
+    SetFlag(cpub, *argA, argB2, result, -1, 2, 2, 2);
+    *argA = result;
 }
 
 void SBC(Cpub *cpub) {
-	Uword *argA, *argB;
-	Uword argB2, cf2;
-	int result = 0;
-	argA = JudgeArgA(cpub);
-	argB = JudgeArgB(cpub);
-	argB2 = Convert2sComplement(*argB);
-	cf2 = Convert2sComplement((Uword) cpub->cf);
-	result = *argA + argB2 + cf2;
-	SetFlag(cpub, *argA, argB2, result, 2, 2, 2, 2);
-	if (cpub->cf) {
-		result -= 0x100;
-	}
-	*argA = result;
+    Uword *argA, *argB;
+    Uword argB2, cf2;
+    int result = 0;
+    argA = JudgeArgA(cpub);
+    argB = JudgeArgB(cpub);
+    argB2 = Convert2sComplement(*argB);
+    cf2 = Convert2sComplement((Uword) cpub->cf);
+    result = *argA + argB2 + cf2;
+    SetFlag(cpub, *argA, argB2 + cf2, result, 2, 2, 2, 2);
+    if (cpub->cf) {
+        result -= 0x100;
+    }
+    *argA = result;
 }
 
 void CMP(Cpub *cpub) {
-	Uword *argA, *argB;
-	Uword argB2;
-	int result = 0;
-	argA = JudgeArgA(cpub);
-	argB = JudgeArgB(cpub);
-	argB2 = Convert2sComplement(*argB);
-	result = *argA + argB2;
-	SetFlag(cpub, *argA, argB2, result, -1, 2, 2, 2);
+    Uword *argA, *argB;
+    Uword argB2;
+    int result = 0;
+    argA = JudgeArgA(cpub);
+    argB = JudgeArgB(cpub);
+    argB2 = Convert2sComplement(*argB);
+    result = *argA + argB2;
+    SetFlag(cpub, *argA, argB2, result, -1, 2, 2, 2);
 }
